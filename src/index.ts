b5e4523fc6f7efc749c8e24595c1c40d930d7162
Tuấn Trần <tuantransn2001@gmail.com>
2024-01-18 09:09:52 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,16 @@ instanceMongodb.connect().catch(console.dir)
 
 const app = express()
 const port = process.env.PORT || 8002
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  methods: 'GET,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+}
 
 app.use(express.json())
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN
-  })
-)
+app.use(cors(corsOptions))
 
 app.use('/api', accessTokenValidator, rootRouter)
 
